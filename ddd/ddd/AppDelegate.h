cb8a3f903f1622f58300b241245a9f0601ebe27f
cowboy007 <chen_yuan_007@163.com>
2017-12-25 10:30:47 +0800

    discription

---
+++ ddd/ddd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ddd
+//
+//  Created by tarena on 2017/12/25.
+//  Copyright © 2017年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
